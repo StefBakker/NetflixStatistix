96692255500f58fcd1d48e3d01a9b4644bacdbba
Stef Bakker <stefbakker@live.nl>
2019-01-14 23:04:54 +0100

    Created GUI interface and Small Database
    Now it is easier to create tabs for the application.

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,8 @@
+import domain.MainForm;
+
 public class Application {
     public static void main(String[] args) {
-        
+        MainForm mainForm = new MainForm();
+        mainForm.setVisible(true);
     }
 }
---
+++ src/domain/MainForm.java
@@ -0,0 +1,16 @@
+package domain;
+
+import javax.swing.*;
+
+public class MainForm extends JFrame {
+
+    private javax.swing.JPanel JPanel;
+
+    public void Mainform() {
+        add(JPanel);
+
+        setTitle("Netflix Statistix");
+        setSize(800,800);
+
+    }
+}
