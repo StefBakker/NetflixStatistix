2c4caf8fd2d9ad6dd6df048c594cf98eb3658170
Unknown <alanvantuyl@gmail.com>
2019-01-17 21:06:42 +0100

    Added attributes to episode and serie class

--- src/datalayer/Episode.java
+++ src/datalayer/Episode.java
@@ -1,4 +1,14 @@
 package datalayer;
 
-public class Episode {
+import java.util.ArrayList;
+
+public class Episode extends Serie {
+    private int length;
+    private String[] episodes;
+    private ArrayList<String[]> episodeData;
+    private int amountOfViews;
+    private int individualProgress;
+
+
+
 }
--- src/datalayer/Serie.java
+++ src/datalayer/Serie.java
@@ -1,4 +1,40 @@
 package datalayer;
 
 public class Serie {
+    private int seriesID;
+    private int minAge;
+    private String title;
+    private String language;
+    private String genre;
+    private int sharedAmountCompleted;
+    private String[] associations;
+    private int amountOfEpisodes;
+
+    public int getMinAge() {
+        return minAge;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getSharedAmountCompleted() {
+        return sharedAmountCompleted;
+    }
+
+    public int getAmountOfEpisodes() {
+        return amountOfEpisodes;
+    }
+
+
 }
+
+
