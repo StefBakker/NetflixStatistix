de651f862fb7bb70b14aacf0ce7b6cbd10ffe2cf
Stef Bakker <stefbakker@live.nl>
2019-01-17 17:44:45 +0100

    [Bugfix] Window opens now in the middle of the screen

--- src/domain/MainForm.java
+++ src/domain/MainForm.java
@@ -3,6 +3,7 @@ package domain;
 import datalayer.DatabaseConnection;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -21,6 +22,9 @@ public class MainForm extends JFrame {
         setTitle("Netflix Statistix");
         setSize(700,400);
 
+        // When form opens center it in the middle of the screen
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         testButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
